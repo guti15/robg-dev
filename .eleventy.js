50f3ab5343dd4e1943b5ca48c7b13dba2cf9dde2
Robert Guti <guti310@gmail.com>
2025-08-27 13:41:14 -0400

    adding new features to improve load time. (#7)

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,46 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "doc": "doc" });
   eleventyConfig.addPassthroughCopy({ "img": "img" });
   eleventyConfig.addPassthroughCopy("src/CNAME");
+
+  // ADD IMAGE OPTIMIZATION HERE
+  eleventyConfig.addNunjucksAsyncShortcode("image", async function(src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [300, 600, 1200],
+      formats: ["webp", "jpeg"],
+      outputDir: "./public/img/optimized/",  // Changed to avoid conflict with your existing img folder
+      urlPath: "/img/optimized/",
+      sharpOptions: {
+        quality: 80
+      }
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes: sizes || "100vw",
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  //  Simpler image shortcode for fixed-width images
+    eleventyConfig.addNunjucksAsyncShortcode("imageFixed", async function(src, alt, width) {
+    let metadata = await Image(src, {
+      widths: [width],
+      formats: ["webp", "jpeg"],
+      outputDir: "./public/img/optimized/",
+      urlPath: "/img/optimized/",
+    });
+
+    let imageAttributes = {
+      alt,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
   
   // Date formatting filter
   eleventyConfig.addFilter("readableDate", dateObj => {
